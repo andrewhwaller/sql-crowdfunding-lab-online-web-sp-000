aa9ad07074c7a7e0a6492f1a25ac27fc2175186d
Andrew Waller <andrewhwaller@gmail.com>
2019-04-13 21:51:50 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,20 +2,40 @@ CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
+<<<<<<< HEAD
   funding_goal NUMERIC,
   start_date TEXT,
   end_date TEXT
 );
+=======
+  goal NUMERIC,
+  start_date TEXT,
+  end_date TEXT
+)
+;
+>>>>>>> 52a0067d40aced4d9fc064eae1c7020cceba8c98
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
+<<<<<<< HEAD
   age INTEGER
 );
+=======
+  age INTEGER,
+)
+;
+>>>>>>> 52a0067d40aced4d9fc064eae1c7020cceba8c98
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
   user_id INTEGER,
   project_id INTEGER,
+<<<<<<< HEAD
   amount INTEGER
 );
+=======
+  amount INTEGER,
+)
+;
+>>>>>>> 52a0067d40aced4d9fc064eae1c7020cceba8c98
